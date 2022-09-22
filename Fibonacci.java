538eb8276c72bda461101e9f93c9eaa555067cd8
tgsvinoth <114044119+tgsvinoth@users.noreply.github.com>
2022-09-22 08:59:35 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int c=0;
+		System.out.println(a);
+		System.out.println(b);
+		for (int i = 1; i < 10; i++) {
+			c=a+b;
+			a=b;
+			b=c;
+			System.out.println(c);
+		}
+	}
+
+}
